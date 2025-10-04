8f99074048c857b1f26c54a760815c51f85be80a
mohamed elgedawy <mohamed.elgedawy100@gmail.com>
2025-10-05 02:49:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Techno_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
